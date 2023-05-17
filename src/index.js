1a84686e56ebb2c6687c0c5af6f51ceb1354015e
정다훈 <ununbium@naver.com>
2023-05-17 22:26:54 +0900

    Upload React data
    리액트 수업 내영 업로드

--- src/index.js
+++ src/index.js
@@ -12,12 +12,14 @@ import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage';
 import AttendanceBook from './chapter_10/AttendanceBook';
 import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 setInterval(() => {
   root.render(
     <React.StrictMode>
-      <SignUp />
+      <Calculator />
+      {/* <SignUp /> */}
       {/* <AttendanceBook /> */}
       {/* <LandingPage /> */}
       {/* <ConfirmButton /> */}
